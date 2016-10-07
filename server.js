b77900e0fec4e84357790200d626cf51994b37ec
Kevin Rouse <kevin.rouse@yale.edu>
2016-10-07 01:28:13 +0000

    add nickname route

--- server.js
+++ server.js
@@ -2,7 +2,17 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response){
-    response.send('Hello MGT-656!');
+    response.send('Hello MGT-656! This is my test');
+});
+
+
+app.get('/ksr43', function(request, response){
+    response.send('Hello MGT-656! Here is my netID');
+});
+
+app.get('/nickname', function(request, response){
+    response.send('graceful-otter');
+    
 });
 
 app.listen(process.env.PORT || 4000);
